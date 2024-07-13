77cab22783d3ae9df7b97287a902f95381678628
Paulo José Zeca <paulozeca@gmail.com>
2024-07-13 19:25:20 -0300

    Conclusão das alterações do desafio

--- tests/controllers/test_product.py
+++ tests/controllers/test_product.py
@@ -100,3 +100,52 @@ async def test_controller_delete_should_return_not_found(client, products_url):
     assert response.json() == {
         "detail": "Product not found with filter: 4fd7cd35-a3a0-4c1f-a78d-d24aa81e7dca"
     }
+
+async def test_controller_create_should_return_success(client, products_url):
+    response = await client.post(products_url, json=product_data())
+
+    content = response.json()
+
+    del content["created_at"]
+    del content["updated_at"]
+    del content["id"]
+
+    assert response.status_code == status.HTTP_201_CREATED
+    assert content == {
+        "name": "Iphone 14 Pro Max",
+        "quantity": 10,
+        "price": "8.500",
+        "status": True,
+    }
+
+async def test_controller_create_should_return_internal_server_error(client, products_url):
+    # Simular erro de inserção enviando dados inválidos
+    response = await client.post(products_url, json={"name": ""})  # Dados inválidos
+
+    assert response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
+    assert response.json() == {"detail": "Error inserting product"}
+    
+async def test_controller_patch_should_return_not_found(client, products_url):
+    response = await client.patch(f"{products_url}invalid-id", json={"price": "7.500"})
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json() == {"detail": "Product with ID invalid-id not found"}
+
+async def test_controller_patch_should_update_updated_at(client, products_url, product_inserted):
+    new_price = "7.500"
+    response = await client.patch(
+        f"{products_url}{product_inserted.id}", json={"price": new_price}
+    )
+
+    content = response.json()
+    updated_at = content.get("updated_at")
+
+    assert response.status_code == status.HTTP_200_OK
+    assert content["price"] == new_price
+    assert updated_at is not None
+
+    # Verificar se updated_at foi atualizado para um horário recente
+    current_time = datetime.utcnow()
+    updated_at_time = datetime.strptime(updated_at, "%Y-%m-%dT%H:%M:%S.%f")
+    assert updated_at_time <= current_time
+    assert (current_time - updated_at_time).total_seconds() < 60  # Atualizado no último minuto
